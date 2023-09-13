7bc7ccc40ed0a81c9d213ea817e7a61a16339230
Mário Cao <kreativermario@gmail.com>
2023-09-13 19:37:35 +0100

    Initial Commit

---
+++ SimpleCalculator-iOS/SimpleCalculator_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculator_iOSApp.swift
+//  SimpleCalculator-iOS
+//
+//  Created by Mário Cao on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculator_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
